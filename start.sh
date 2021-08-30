48e9574220096c05c7b6876b151d077e3f5f53d4
MaybeLaterUKnow <87666237+MaybeLaterUKnow@users.noreply.github.com>
2021-08-31 06:31:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-curl "https://github.com/NezuChan/Lavalink/releases/download/v3.3.1/Lavalink.jar" -o Lavalink.jar
+curl "https://github.com/freyacodes/Lavalink/releases/download/3.3.2.5/Lavalink.jar" -o Lavalink.jar
 
 java -jar Lavalink.jar
